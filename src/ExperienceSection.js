0b067a29cbe187811a484134f2e6bc931918e6dd
tbruner <trevor.a.bruner@gmail.com>
2022-10-09 20:31:32 -0400

    Add Experience Section

---
+++ src/ExperienceSection.js
@@ -0,0 +1,24 @@
+class ExperienceSection extends React.Component {
+  render() {
+    return (
+      <>
+        <form>
+        <label for='employer'>Employer Name</label>
+        <input type='text' id='employer' required />
+
+        <label for='job-title'>Job Title</label>
+        <input type='text' id='job-title' required />
+
+        <label for='job-date-from'>From</label>
+        <input type='date' id='job-date-from' required />
+
+        <label for='date-to'>To</label>
+        <input type='date' id='job-date-to' required />
+
+        <label for='job-duties'>Job Duties</label>
+        <input type='text' id='job-duties' required />
+        </form>
+      </>
+    );
+  }
+}
