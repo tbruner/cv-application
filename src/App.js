c57178b52700a0e323521a2563e020bd82f6050a
tbruner <trevor.a.bruner@gmail.com>
2022-10-20 20:43:24 -0400

    Clean up file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import './App.css';
-import GeneralInfoSection from './GenInfoSection.js';
+import './styles/App.css';
+import GeneralInfoSection from './components/GenInfoSection.js';
+import Navigation from './components/Navigation.js';
+import Form from './components/Form.js';
+import Display from './components/Display.js'
 
 function App() {
   return (
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,5 @@
+import '../styles/Navigation.css'
+
 function Navigation() {
   return (
     <>
